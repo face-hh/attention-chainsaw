44f57ff319f1a8cccdf2e02d4460cec849287990
Face <xxfacexc@gmail.com>
2022-03-01 16:30:56 +0200

    [FIX] Fixed license not detecting on submit 	modified:   src/static/css/main.css 	modified:   src/static/html/index.html 	new file:   src/static/storage/licenses.js

---
+++ src/static/storage/licenses.js
@@ -0,0 +1,20 @@
+module.exports = [
+    'Microsoft Reciprocal License',
+    'The Code Project Open License(CPOL)',
+    'The Common Development and Distribution License(CDDL)',
+    'The Microsoft Public License(Ms - PL)',
+    'The Mozilla Public License 1.1(MPL 1.1)',
+    'The Common Public License Version 1.0(CPL)',
+    'The Eclipse Public License 1.0',
+    'The MIT License',
+    'The BSD License',
+    'The Apache License, Version 2.0',
+    'The Creative Commons Attribution - ShareAlike 2.5 License',
+    'The zlib / libpng License',
+    'A Public Domain dedication',
+    'The Creative Commons Attribution 3.0 Unported License',
+    'The Creative Commons Attribution - Share Alike 3.0 Unported License',
+    'The Creative Commons Attribution - NoDerivatives 3.0 Unported',
+    'The GNU Lesser General Public License(LGPLv3)',
+    'The GNU General Public License(GPLv3)'
+]
