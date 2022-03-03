60f9c35ae21b3152254e891cafba61a6a1b2cf90
Face <69168154+face-hh@users.noreply.github.com>
2022-03-03 21:05:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const numbers = [
+	[
+		' ____ ',
+		'/_   |',
+		' |   |',
+		' |   |',
+		' |___|',
+		'	  ',
+	],
+	[
+		'________  ',
+		'\\_____  \\ ',
+		' /  ____/ ',
+		'/       \\ ',
+		'\\_______ \\ ',
+		'	\\/',
+	],
+	[
+		'________  ',
+		'\\_____  \\ ',
+		'  _(__  < ',
+		' /       \\ ',
+		'/______  /',
+		'	\\/ ',
+	],
+];
+
 (async () => {
 	const readline = require('readline');
 
@@ -19,12 +46,53 @@
 	if(!name || !pass) return console.log('No name or password was provided.');
 	console.log(
 		`\n\nHello \x1b[32m${name}\x1b[0m, welcome to \x1b[31mAttention, chainsaw!\x1b[0m\n` +
-        '  \x1b[33m1)\x1b[0m Shut the fuck up',
+        '  \x1b[33m1)\x1b[0m Boss fight',
 	);
 
 	const response = await askQuestion('> ');
 
-	if(response === '1') return console.log('ok i go');
+	if(response === '1') {
+		console.clear();
+		console.log('\x1b[31mBoss fight is starting');
+
+		let i = 0;
+
+		const interval = setInterval(async () => {
+			if(i === 3) {
+				clearInterval(interval);
+
+				const gameData = [
+					{ hp: 15, dmg: 4, turn: false },
+					{ hp: 10, dmg: 2, turn: true },
+				];
+
+				const gameIsRunning = true;
+
+				console.clear();
+
+				console.log(
+					`Rachmolotov - \x1b[31m❤️ ${gameData[0].hp}\x1b[0m | \x1b[34m🔪 ${gameData[0].dmg}\x1b[0m` +
+					'                   //\\\\                   ' +
+					`${name} - \x1b[31m❤️ ${gameData[1].hp}\x1b[0m | \x1b[34m🔪 ${gameData[1].dmg}\x1b[0m`,
+				);
+
+				let decission = await askQuestion('(A)ttack | (I)nsult | (D)efend: ');
+
+				if(!['a', 'i', 'd', 'attack', 'insult', 'defend'].includes(decission.toLowerCase())) {
+					console.log('Invalid input!');
+					decission = await askQuestion('(A)ttack | (I)nsult | (D)efend: ');
+				}
+
+			}
+
+			console.clear();
+			console.log('\x1b[31mBoss fight is starting');
+			console.log(numbers[i].join('\n') + '\x1b[0m');
+
+			i++;
+		}, 1000);
+		return;
+	}
 
 	return console.log('Invalid option!');
 })();
